ac3851c19495417b3d876822f29816cf96e98022
Conifer <64596327+conifer-dev@users.noreply.github.com>
2021-05-11 08:47:52 +0100

    Update sound.swift

--- Sources/Swift-Pong/src/sound.swift
+++ Sources/Swift-Pong/src/sound.swift
@@ -2,10 +2,11 @@ import Foundation
 import CRaylib
 import Raylib
 
+let filepath = Bundle.main.path(forResource: "bgm", ofType: "wav")
 
 struct SoundManager {
-    private static var backgroundMusicPath = URL(string: "./src/Assets/Sounds/bgm.wav")!
-    private static var paddleHitPath = URL(string: "./src/Assets/Sounds/paddleHit.wav")!
+    private static var backgroundMusicPath = URL(string: "src/Assets/Sounds/bgm.wav")!
+    private static var paddleHitPath = URL(string: "src/Assets/Sounds/paddleHit.wav")!
     static var backgroundMusic = LoadMusicStream(backgroundMusicPath.path)
     static var paddleHit = LoadSound(paddleHitPath.path)
 
